308a7655e030a3246bfa03d1e826f80cfb0f120e
dqn <suyarizzz@gmail.com>
2019-08-01 23:27:03 +0900

    refacor: calc.js

--- calc.js
+++ calc.js
@@ -1,162 +1,106 @@
-'use strict'
+'use strct'
 
-const printTree = (node, depth=0) => {
-  if (!node) {
-    return;
-  }
+const isNumber = c =>
+	/\d+/.test(c);
 
-  console.log(new Array(depth).fill().map(_ => '-').join('') + node.value);
+const isBlank = c =>
+	(c === ' ');
 
-  printTree(node.left, depth + 1);
-  printTree(node.right, depth + 1);
+const tokenize = string => {
+	let temp = '';
+	const tokens = [];
+	for (const c of string) {
+		if (isNumber(c)) {
+			temp += c;
+			continue;
+		}
+		if (isBlank(c)) {
+			continue;
+		}
+		tokens.push(temp);
+		tokens.push(c);
+		temp = '';
+	}
+	return [...tokens, temp];
 };
 
-const printAssembly = node => {
-  if (isNumber(node.value)) {
-    console.log(`  push ${node.value}`);
-    return
-  }
-
-  printAssembly(node.left)
-  printAssembly(node.right);
-
-  console.log('  pop rdi');
-  console.log('  pop rax');
-
-  switch (node.value) {
-    case '+':
-      console.log('  add rax, rdi');
-      break;
-    case '-':
-      console.log('  sub rax, rdi');
-      break;
-    case '*':
-      console.log('  imul rax, rdi');
-      break;
-    case '/':
-      console.log('  cqo');
-      console.log('  idiv rdi');
-      break;
-  }
-
-  console.log('  push rax');
-};
-
-const calcTree = node => {
-  if (isNumber(node.value)) {
-    return Number(node.value);
-  }
-
-  const [v1, v2] = [calcTree(node.left), calcTree(node.right)];
-
-  switch (node.value) {
-    case '+': return v1 + v2;
-    case '-': return v1 - v2;
-    case '*': return v1 * v2;
-    case '/': return v1 / v2;
-  }
-};
-
-const makeNode = (value, left, right) => ({
-  value, left, right,
-});
-
-const isNumber = text =>
-  text.split('').every(c => '0123456789'.includes(c));
-
-const isSpace = c =>
-  (c === ' ');
-
-const tokenize = text => {
-  const tokens = [];
-  let temp = '';
-
-  for (const c of text) {
-    if (isNumber(c)) {
-      temp += c;
-      continue;
-    }
-    if (isSpace(c)) {
-      continue;
-    }
-    tokens.push(temp);
-    tokens.push(c);
-    temp = '';
-  }
-
-  return [...tokens, temp];
+const number = tokens => {
+	return { value: Number(tokens.shift()) };
 };
 
 const term = tokens => {
-  if (isNumber(tokens[0])) {
-    return makeNode(tokens.shift(), null, null);
-  }
-  return expr(tokens);
+	let node = number(tokens);
+	while (true) {
+		switch (tokens[0]) {
+		case '*':
+		case '/':
+			node = { value: tokens.shift(), left: node, right: number(tokens) };
+			break;
+		default:
+			return node;
+		}
+	}
 };
 
-const mul = tokens => {
-  let node = term(tokens);
-
-  while (true) {
-    const token = tokens[0];
-    switch (token) {
-      case '*':
-      case '/':
-        tokens.shift();
-        node = makeNode(token, node, term(tokens));
-        break;
-      default:
-        return node;
-    }
-  }
+const expression = tokens => {
+	let node = term(tokens);
+	while (true) {
+		switch (tokens[0]) {
+		case '+':
+		case '-':
+			node = { value: tokens.shift(), left: node, right: term(tokens) };
+			break;
+		default:
+			return node;
+		}
+	}
 };
 
-const expr = tokens => {
-  let node = mul(tokens);
+const printAssembly = node => {
+	if (!node.left || !node.right) {
+		console.log(`  push ${node.value}`);
+		return
+	}
 
-  while (true) {
-    const token = tokens[0];
-    switch (token) {
-      case '+':
-      case '-':
-        tokens.shift();
-        node = makeNode(token, node, mul(tokens));
-        break;
-      default:
-        return node;
-    }
-  }
-};
+	printAssembly(node.left);
+	printAssembly(node.right);
 
-const execute = text => {
-  const tokens = tokenize(text);
+	console.log(`  pop rdi`);
+	console.log(`  pop rax`);
 
-  // console.log(tokens);
+	switch (node.value) {
+		case '+':
+			console.log(`  add rax, rdi`);
+			break;
+		case '-':
+			console.log(`  sub rax, rdi`);
+			break;
+		case '*':
+			console.log(`  imul rax, rdi`);
+			break;
+		case '/':
+			console.log('  cqo')
+			console.log(`  idiv rdi`);
+			break;
+	}
 
-  const root = expr(tokens);
-
-  console.log('.intel_syntax noprefix');
-  console.log('.global main');
-  console.log('  main:');
-
-  printAssembly(root);
-
-  console.log('  pop rax');
-  console.log('  ret');
-
-  // printTree(root);
-  // console.log(calcTree(root));
+	console.log('  push rax');
 };
 
 const main = argv => {
-  if (argv.length !== 3) {
-    console.error('Usage: node calc.js <expression>');
-    return 1;
-  }
+	const tokens = tokenize(argv[2]);
+	const node = expression(tokens);
 
-  execute(argv[2]);
+	console.log('.intel_syntax noprefix');
+	console.log('.global main');
+	console.log('main:');
+
+	printAssembly(node);
+
+	console.log('  pop rax');
+	console.log('  ret');
 };
 
 if (require.main === module) {
-  main(process.argv);
+	main(process.argv);
 }
